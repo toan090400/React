2e63190ccbd2bbec451a0db035674815e92cf12e
nductoan <toanbeo090400@gmail.com>
2022-06-29 15:41:18 +0700

    Done 04

---
+++ ui-react/4/src/components/Exam/Exam.jsx
@@ -0,0 +1,45 @@
+import ExamItem from './ExamItem';
+import Cart from '../UI/Cart'
+import ExamFilter from './ExamFilter';
+import './Exam.css'
+
+import { useState } from 'react';
+
+const Exam = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2019');
+
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    };
+    return (
+        <Cart>
+            <div>
+                <ExamFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+                <ExamItem
+                    title={props.items[0].title}
+                    amount={props.items[0].amount}
+                    date={props.items[0].date}
+                />
+                <ExamItem
+                    title={props.items[1].title}
+                    amount={props.items[1].amount}
+                    date={props.items[1].date}
+                />
+                <ExamItem
+                    title={props.items[2].title}
+                    amount={props.items[2].amount}
+                    date={props.items[2].date}
+                />
+                <ExamItem
+                    title={props.items[3].title}
+                    amount={props.items[3].amount}
+                    date={props.items[3].date}
+                />
+            </div>
+        </Cart>
+        
+
+    );
+}
+ 
+export default Exam;

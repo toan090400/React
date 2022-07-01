25c07d1e54843ada064bf50f9038458269f7315d
nductoan <toanbeo090400@gmail.com>
2022-07-01 13:39:21 +0700

    Done 7

--- ui-react/07/src/App.js
+++ ui-react/07/src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import Exam from './components/Exam/Exam';
+import NewExam from './components/NewExam/NewExam'
 
-function App() {
+import { useState } from 'react';
+
+const DUMMY_EXPENSES = [
+  {
+    id: '1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: '2',
+    title: 'New TV',
+    amount: 799.49,
+    date: new Date(2021, 2, 12)
+  },
+  {
+    id: '3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: '4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  const onAddExpense = (data) => {
+    setExpenses([data,...expenses])
+  }
+  const onDelete = (data) => {
+    console.log(data)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NewExam onAddExpense={onAddExpense}/>
+      <Exam items={expenses} onDeleteExam={onDelete}/>
     </div>
+    
   );
 }
 
---
+++ ui-react/07/src/components/Exam/Exam.jsx
@@ -0,0 +1,43 @@
+import ExamItem from './ExamItem';
+import Cart from '../UI/Cart'
+import ExamFilter from './ExamFilter';
+import './Exam.css'
+
+import { useState } from 'react';
+
+const Exam = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2020');
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    };
+    
+    const filteredExpenses = props.items.filter((expense) => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+    const onHandleDetete = (value) => {
+        // console.log(value)
+        props.onDeleteExam(value)
+    }
+    return (
+        <Cart>
+            <div>
+                <ExamFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+                {filteredExpenses.length === 0 ? 
+                    <p>No Item</p>
+                    :
+                    filteredExpenses.map((item) => (
+                        <ExamItem key={item.id}
+                            title={item.title}
+                            amount={item.amount}
+                            date={item.date}
+                            id={item.id}
+                            Delete={onHandleDetete}
+                        />
+                    ))
+                }
+            </div>
+        </Cart>
+    );
+}
+ 
+export default Exam;

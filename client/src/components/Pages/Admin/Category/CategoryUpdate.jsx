15e088093b8ef874a8b818e3e9e76f11267904b3
nductoan <toanbeo090400@gmail.com>
2022-07-12 16:27:42 +0700

    useForm Update catrgory is done

--- client/src/components/Pages/Admin/Category/CategoryUpdate.jsx
+++ client/src/components/Pages/Admin/Category/CategoryUpdate.jsx
@@ -49,34 +49,40 @@ const CategoryUpdate = () => {
                 <h2>Thông báo: {getMessage}</h2>
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <div>
-                        <h4>name: {getData.name}</h4>
+                        <h4>name</h4>
                     </div>
                     <div className="">
                         <label htmlFor="name">new name</label>
-                        <input type="text" id="name" 
-                            {...register("name",{
-                                required: {
-                                    value: true,
-                                    message: "name không được trống"
-                                },
-                            })}
-                        />
+                        {   getData.name && 
+                            <input type="text" id="name" 
+                                {...register("name",{
+                                    required: {
+                                        value: true,
+                                        message: "name không được trống"
+                                    },
+                                })}
+                                defaultValue={getData.name}
+                            />
+                        }
                         {errors.name && (<p>{errors.name.message}</p>)}
                     </div>
                     <div>
-                        <h4>description: {getData.description}</h4>
+                        <h4>description</h4>
                     </div>
                     <div className="">
                         
                         <label htmlFor="description">new description</label>
-                        <input type="text" id="description"
-                            {...register("description",{
-                                required: {
-                                    value: true,
-                                    message: "description không được trống"
-                                },
-                            })}
-                        />
+                        {   getData.description && 
+                            <input type="text" id="description" 
+                                {...register("description",{
+                                    required: {
+                                        value: true,
+                                        message: "description không được trống"
+                                    },
+                                })}
+                                defaultValue={getData.description}
+                            />
+                        }
                         {errors.description && (<p>{errors.description.message}</p>)}
                     </div>
                     
